64cb8244a8e6ca212906c697083796d45a6606c4
shag12 <shag12@mail.ru>
2021-01-17 00:14:33 +0300

    Com3

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,27 @@
 #include <ArduinoJson.h>
 
 
+#include <WiFi.h>
+extern "C" {
+	#include "freertos/FreeRTOS.h"
+	#include "freertos/timers.h"
+}
+#include <AsyncMqttClient.h>
+
+#define MQTT_HOST IPAddress(178, 213, 114, 222)
+#define MQTT_PORT 1883
+
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+
+const char* mqtt_user = "shag";
+const char* mqtt_pass = "Assasin12";
+const char* mqtt_topic = "shag/ESP-HOME/sensors/#";
+
+
+
+
+
 #define USE_SERIAL Serial
 
 
@@ -69,6 +90,25 @@ void h_get();
 void h_get2();
 
 
+//MQTT
+
+void connectToMqtt();
+void onMqttConnect(bool sessionPresent);
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason);
+void onMqttSubscribe(uint16_t packetId, uint8_t qos);
+void onMqttUnsubscribe(uint16_t packetId);
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total);
+void onMqttPublish(uint16_t packetId);
+
+//MQTT END
+
+
+
+
+
+
+
+
 
 
 
@@ -90,6 +130,16 @@ void setup() {
     delay(1000);
   }
   
+  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onSubscribe(onMqttSubscribe);
+  mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+
   
 }
 
@@ -188,6 +238,9 @@ void dacha() {
    ez.screen.clear();
    ez.header.show("Dacha");
    ez.buttons.show("#" + exit_button + "#");
+
+  void connectToMqtt();
+
   while(true) {
  
     h_get2();
@@ -422,4 +475,77 @@ void h_get2() {
 
   http.end();
 }
+
+
+// MQTT!!!
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+  uint16_t packetIdSub = mqttClient.subscribe(mqtt_topic, 2);
+  Serial.print("Subscribing at QoS 2, packetId: ");
+  Serial.println(packetIdSub);
+  //mqttClient.publish("test/lol", 0, true, "test 1");
+  //Serial.println("Publishing at QoS 0");
+  //uint16_t packetIdPub1 = mqttClient.publish("test/lol", 1, true, "test 2");
+  //Serial.print("Publishing at QoS 1, packetId: ");
+  //Serial.println(packetIdPub1);
+  //uint16_t packetIdPub2 = mqttClient.publish("test/lol", 2, true, "test 3");
+  //Serial.print("Publishing at QoS 2, packetId: ");
+  //Serial.println(packetIdPub2);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  Serial.println("Publish received.");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
 
